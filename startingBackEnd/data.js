155fa9b74a6df73086f1d146998eb12404252604
Everton <evertonaugusto1997@gmail.com>
2020-05-17 23:44:52 -0300

    Creating data.js page and updating server.js

---
+++ startingBackEnd/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário Animado cm JS puroe CSS | Mayk Brito.",
+    duration: "57 min",
+    price: "Free",
+  },
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário Animado cm JS puroe CSS | Mayk Brito.",
+    duration: "57 min",
+    price: "Free",
+  },
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário Animado cm JS puroe CSS | Mayk Brito.",
+    duration: "57 min",
+    price: "Free",
+  },
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário Animado cm JS puroe CSS | Mayk Brito.",
+    duration: "57 min",
+    price: "Free",
+  },
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário Animado cm JS puroe CSS | Mayk Brito.",
+    duration: "57 min",
+    price: "Free",
+  },
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário Animado cm JS puroe CSS | Mayk Brito.",
+    duration: "57 min",
+    price: "Free",
+  },
+];
