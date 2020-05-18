aaa570f219c070e4d87a84af2379e61395eb9753
Everton <evertonaugusto1997@gmail.com>
2020-05-18 01:57:56 -0300

    page updating about

--- startingBackEnd/server.js
+++ startingBackEnd/server.js
@@ -10,10 +10,34 @@ server.set("view engine", "njk");
 
 nunjucks.configure("views", {
   express: server,
+  autoescape: false,
 });
 
 server.get("/", function (req, res) {
-  return res.render("about");
+  const about = {
+    avatar_url:
+      "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+    name: "Mayk brito",
+    role: "Instrutor - Rocketseat",
+    description: `Programador full-stack, focado em ensinar pela
+      <a href="https://rocketseat.com.br/">Rocketseat </a>`,
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/maykbrito",
+      },
+      {
+        name: "Twitter",
+        url: "https://twitter.com/maykbrito",
+      },
+      {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/maykbrito/",
+      },
+    ],
+  };
+
+  return res.render("about", { about });
 });
 
 server.get("/classes", function (req, res) {
